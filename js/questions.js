0a5013bf5ad9f8e0e8d4fc86ad5dcd5a89953250
monikasuryawanshi2015 <monikasuryawanshi2015@gmail.com>
2024-07-08 15:26:26 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "1) What is the maximum possible length of an identifier?",
+  answer: "None of these above",
+  options: [
+    "16",
+    "None of these above",
+    "32",
+    "64"
+  ]
+},
+  {
+  numb: 2,
+  question: "Who developed the Python language?",
+  answer: "Guido van Rossum",
+  options: [
+    "wick vam rossum",
+    "zem dem",
+    "Niene Stom",
+    "Guido van Rossum"
+  ]
+},
+  {
+  numb: 3,
+  question: " In which year was the Python language developed?",
+  answer: "1989",
+  options: [
+    "1989",
+    "1980",
+    "1995",
+    "1992"
+  ]
+},
+  {
+  numb: 4,
+  question: " What do we use to define a block of code in Python language?",
+  answer: "Indentation",
+  options: [
+    "key",
+    "brcket",
+    "none of these",
+    "Indentation"
+  ]
+},
+  {
+  numb: 5,
+  question: "Which character is used in Python to make a single line comment?",
+  answer: "#",
+  options: [
+    "#",
+    "//",
+    "*",
+    "@"
+  ]
+},
+// you can uncomment the below codes and make duplicate as more as you want to add question
+// but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+//   {
+//   numb: 6,
+//   question: "Your Question is Here",
+//   answer: "Correct answer of the question is here",
+//   options: [
+//     "Option 1",
+//     "option 2",
+//     "option 3",
+//     "option 4"
+//   ]
+// },
+];
